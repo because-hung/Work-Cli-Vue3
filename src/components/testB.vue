2dc5c6c199fc1ede8990f6ea855d8ca4ad29fc8a
Becasuse-hung <4a33a006@stust.edu.tw>
2022-03-10 05:43:10 +0800

    tt1

--- src/components/testB.vue
+++ src/components/testB.vue
@@ -3,6 +3,7 @@
    <button @click="changeEve(true)">BBBTTT</button>
    <button @click="changeEve(false)">BBBFFF</button>
     <h2>vx:{{store.state.statusS}}</h2>
+    <h3>test git</h3>
 </template>
 <script setup>
 import { useStore } from 'vuex'
