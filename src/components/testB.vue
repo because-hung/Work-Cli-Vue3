df5b031b61a4a9cb3d839d4ffa3cb5566aeb9872
Becasuse-hung <4a33a006@stust.edu.tw>
2022-03-16 23:19:24 +0800

    gogogogog

--- src/components/testB.vue
+++ src/components/testB.vue
@@ -8,10 +8,19 @@
     <h3>test git3</h3>-->
 </template>
 <script setup>
+import { reactive } from '@vue/runtime-core'
 import { useStore } from 'vuex'
 const store = useStore()
 const changeEve = (ccc) => {
   store.dispatch('getStatus', ccc)
   localStorage.setItem('ccc', ccc)
 }
+const CCC = reactive({
+  id: '10',
+  name: 'tom'
+})
+
+const AAA = reactive(JSON.parse(JSON.stringify(CCC)))
+console.log('cc', CCC)
+console.log('aaa', AAA)
 </script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,24 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script setup>
+// restart
+const date = Date.now()
+const today = new Date(date)
+console.log(today)
+
+const enDate = Date.parse(new Date('2012/05/17 09:20'))
+const twDate = Date.parse(new Date('2012/05/16 10:00'))
+console.log(enDate)
+console.log(twDate)
+const x = enDate - twDate
+console.log(x / 60 / 60 / 1000)
+
+const Ary = [1, 2, 3, 4, 5]
+
+Ary.splice(2, 1, 6)
+
+// 1, 2, 6, 4, 5
+console.log(Ary)
+
+</script>
